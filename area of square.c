b9310bec662ee75574f461144a3a10836cb6f77e
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-10 17:27:37 +0530

    Add files via upload
    Area of square

---
+++ area of square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int s,area;
+printf("enter the length of side");
+scanf("%d",&s);
+area=5*5;
+printf("area of square=%d",area);
+getch();
+}

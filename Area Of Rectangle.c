0925bf104dfa087ca471048dbebb254e3c3bf3c5
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-11 07:45:12 +0530

    Add files via upload

---
+++ Area Of Rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int l,b,area;
+printf("enter the value of l and b");
+scanf("%d %d",&l,&b);
+area=l*b;
+printf("area of rectangle=%d",area);
+getch();
+}

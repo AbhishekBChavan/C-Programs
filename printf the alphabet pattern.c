4c6d2d145d4f9874dddf402418eb631a451d4042
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-17 20:43:30 +0530

    Add files via upload

---
+++ printf the alphabet pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+char i,j;
+for(i='a'; i<='e'; i++)
+{
+for(j='a'; j<='e'; j++)
+{
+printf("%c",i);
+}
+printf("\n");
+}
+getch();
+}

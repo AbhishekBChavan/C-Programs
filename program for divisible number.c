2eec0c93bc13f7bc042021fe4e47c9566f2f5fc8
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-19 20:50:51 +0530

    Add files via upload

---
+++ program for divisible number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a;
+    printf("enter any number");
+    scanf("%d",&a);
+    if(a%8==0)
+    {
+        printf("divisible no");
+    }
+    getch();
+}

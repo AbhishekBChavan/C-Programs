64f268fcaee6546302c897cd3d60eb8a7b5c7386
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-20 07:17:58 +0530

    Add files via upload

---
+++ happy diwali program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int i;
+    textcolor(RED);
+    textbackground(WHITE);
+    for(i=1; i<160; i++)
+    {
+        cprintf("HAPPY DIWALI \3 ");
+    }
+    getch();
+}

b2f73da51783d7228a9329457b4df932579971e3
AbhishekBChavan <abhishekchavan2682@gmail.com>
2025-10-25 20:54:19 +0530

    Add files via upload

---
+++ smallest and largest number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+printf("enter any number");
+scanf("%d",&a);
+if(a>10)
+{
+printf("larget no");
+}
+else
+{
+printf("smallest no");
+}
+getch();
+}
